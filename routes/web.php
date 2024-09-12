8db1f4e28d2b2ff7ccaf1b5bb48f687cce414c65
mj23-coder <calupcupan.marcjustin.bsit@gmail.com>
2024-09-13 01:38:02 +0800

    sheesh1

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome');  // Home page
+})->name('home');
+
+Route::get('/about', function () {
+    return view('about');  // About Us page
+})->name('about');
